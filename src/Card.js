591a1b865995e28cb9b3a53e242a06467e17b296
PriyankaRC16 <rupali317@gmail.com>
2023-12-16 16:18:05 +0800

    Misc info

--- src/Card.js
+++ src/Card.js
@@ -11,4 +11,4 @@ export default function Card() {
 }
 
 /* I have spent ard 3h so far to understand npm, node,homebrew, nvm, documentation related to create react app */
-/* I have spend 5h 41m for writing the react code using create-react-app and for writing sass code, figuring out github and how to do live, how to push locally to remote*/
+/* I have spend 6h 19m +  for writing the react code using create-react-app and for writing sass code, figuring out github and how to do live, how to push locally to remote*/
