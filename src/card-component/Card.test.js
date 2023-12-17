3ad18635ea7a8fccb9463c008b5e32c34c2d2e7f
PriyankaRC16 <rupali317@gmail.com>
2023-12-17 15:30:21 +0800

    Renamed to better names

--- src/card-component/Card.test.js
+++ src/card-component/Card.test.js
@@ -5,14 +5,14 @@ describe('Card component', () => {
 
   test('renders card heading text', () => {
     render(<Card />);
-    const textElement = screen.getByText(/Improve your front-end skills by building projects/i);
-    expect(textElement).toBeInTheDocument();
+    const headingTextElement = screen.getByText(/Improve your front-end skills by building projects/i);
+    expect(headingTextElement).toBeInTheDocument();
   });
   
   test('renders card sub text', () => {
       render(<Card />);
-      const textElement = screen.getByText(/Scan the QR code to visit Frontend Mentor and take your coding skills to the next level/i);
-      expect(textElement).toBeInTheDocument();
+      const subTextElement = screen.getByText(/Scan the QR code to visit Frontend Mentor and take your coding skills to the next level/i);
+      expect(subTextElement).toBeInTheDocument();
   });
-  
+
 });
